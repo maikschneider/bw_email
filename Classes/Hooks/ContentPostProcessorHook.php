35ca5bc3c3ecd02fdeef04e0d06663327765053d
Maik Schneider <m.schenider@blueways.de>
2018-12-01 14:54:26 +0100

    feat: inline css

--- Classes/Hooks/ContentPostProcessorHook.php
+++ Classes/Hooks/ContentPostProcessorHook.php
@@ -3,27 +3,96 @@
 namespace Blueways\BwEmail\Hooks;
 
 use Hampe\Inky\Inky;
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ContentPostProcessorHook
 {
+
+    /**
+     * @param $parameters
+     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
+     */
     public function noCache(&$parameters)
     {
-
+        /** @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $pobj */
         $pobj = $parameters['pObj'];
         $page = $pobj->page;
-        if($page['doktype'] !== 117) {
-           return;
+        if ($page['doktype'] !== 117) {
+            return;
         }
 
+        $this->inkyMarkup($pobj);
+        $this->inlineCss($pobj);
+        $this->cleanHeadTag($pobj);
+        //\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($parameters, __LINE__ . ' in ' . __CLASS__);
+    }
+
+    /**
+     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $pobj
+     */
+    private function inkyMarkup($pobj)
+    {
         $gridColumns = 12; //optional, default is 12
         $additionalComponentFactories = []; //optional
         $inky = new Inky($gridColumns, $additionalComponentFactories);
 
-        $pobj->content = $inky->releaseTheKraken($pobj->content);
-
-        //\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($pobj, __LINE__ . ' in ' . __CLASS__);
+        try {
+            $pobj->content = $inky->releaseTheKraken($pobj->content);
+        } catch (\PHPHtmlParser\Exceptions\CircularException $e) {
+        }
     }
 
+    /**
+     * @return array
+     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
+     */
+    private function loadAllTypoScriptSetup()
+    {
+        /** @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager $configurationManager */
+        $configurationManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager');
+        $typoscript = $configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+
+        return $typoscript['page.'];
+    }
+
+    /**
+     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $pobj
+     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
+     */
+    private function inlineCss($pobj)
+    {
+        $typoscript = $this->loadAllTypoScriptSetup();
+        $cssFiles = $typoscript['includeCSS.'];
+        $css = '';
+        foreach ($cssFiles as $cssFile) {
+            $cssFilePath = GeneralUtility::getFileAbsFileName($cssFile);
+            $css .= file_get_contents($cssFilePath);
+        }
+
+        $cssToInlineStyles = new CssToInlineStyles();
+        $pobj->content = $cssToInlineStyles->convert(
+            $pobj->content,
+            $css
+        );
+    }
+
+    /**
+     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $pobj
+     */
+    protected function cleanHeadTag($pobj)
+    {
+        // remove stylesheet tags
+        $pobj->content = preg_replace(
+            '/<link (?="[^">]*rel=\s*[\'"]stylesheet[\'"])(?![^>]*href=\s*[\'"]http)[^>]*>/i',
+            '',
+            $pobj->content
+        );
+    }
+
+    /**
+     * @param $parameters
+     */
     public function cache(&$parameters)
     {
         $pobj = $parameters['pObj'];
@@ -31,7 +100,5 @@ class ContentPostProcessorHook
         if ($page['doktype'] !== 117) {
             return;
         }
-
-        // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($parameters, __LINE__ . ' in ' . __CLASS__ );
     }
 }
--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,6 +2,8 @@
 defined('TYPO3_MODE') || die('Access denied');
 
 call_user_func(
+    'bw_email',
+    'pages',
     function ($extKey, $table) {
         $emailDoktype = 117;
 
@@ -41,8 +43,5 @@ call_user_func(
         $GLOBALS['TCA']['pages']['types']['117']['showitem'] = '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.standard;standard,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.title;title,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.metadata,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.abstract;abstract,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.metatags;metatags,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.editorial;editorial,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.appearance,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.layout;layout,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.replace;replace,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.behaviour,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.links;links,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.caching;caching,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.miscellaneous;miscellaneous,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.module;module,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.resources,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.media;media,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.config;config,--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.language;language,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.visibility;visibility,--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.access;access,--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,--div--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category,categories,--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended
 ';
         $GLOBALS['TCA']['pages']['types']['117']['columnsOverrides']['layout']['config']['type'] = 'passthrough';
-
-    },
-    'bw_email',
-    'pages'
+    }
 );
