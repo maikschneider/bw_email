ef5b6d9ad77db2e18be8459548a4890d37fb81d6
Maik Schneider <m.schneider@blueways.de>
2019-11-19 10:25:06 +0100

    feat: register module, AdministrationController

---
+++ Classes/Controller/AdministrationController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Blueways\BwEmail\Controller;
+
+class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    public function indexAction()
+    {
+
+    }
+}
